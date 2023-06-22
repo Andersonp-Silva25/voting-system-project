c68e785dd3f7d249c9b7d81f8b810969233b478f
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-06-22 16:09:28 -0300

    :sparkles: Feat: Adding the voto attribute to the PessoaCandidata class

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -6,10 +6,15 @@ package com.trybe.acc.java.sistemadevotacao;
 public class PessoaCandidata extends Pessoa {
 
   private int numero;
+  private int votos;
 
+  /**
+   * Construtor da classe PessoaCandidata.
+   */
   public PessoaCandidata(String nome, int numero) {
     super(nome);
     this.numero = numero;
+    this.votos = 0;
   }
 
   public int getNumero() {
@@ -18,6 +23,18 @@ public class PessoaCandidata extends Pessoa {
 
   public void setNumero(int numero) {
     this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
   }
 
 }
